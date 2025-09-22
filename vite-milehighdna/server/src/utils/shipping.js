de46b98fd910904f008dbdbda6f98be96adaefa6
Khalid Morris <kha.morris@gmail.com>
2025-09-21 18:46:19 -0600

    Update utils

--- vite-milehighdna/server/src/utils/shipping.js
+++ vite-milehighdna/server/src/utils/shipping.js
@@ -1,5 +1,15 @@
-// server/src/utils/shipping.js
-import rates from "../config/shippingRates.json" assert { type: "json" };
+ // server/src/utils/shipping.js
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Resolve __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Load JSON config once
+const ratesPath = path.join(__dirname, "../config/shippingRates.json");
+const rates = JSON.parse(fs.readFileSync(ratesPath, "utf-8"));
 
 export async function getShippingFee(type, countryCode) {
   try {
