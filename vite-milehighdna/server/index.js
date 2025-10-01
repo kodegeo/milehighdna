48dd11b5e1447f2c9834213c1156b2e0c4d712e6
Khalid Morris <kha.morris@gmail.com>
2025-10-01 08:50:45 -0700

    Fix webhook setup

--- vite-milehighdna/server/index.js
+++ vite-milehighdna/server/index.js
@@ -3,6 +3,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import checkoutRoutes from "./src/routes/checkoutRoutes.js";
+import bodyParser from "body-parser";
+
 
 
 // ✅ Load env first
