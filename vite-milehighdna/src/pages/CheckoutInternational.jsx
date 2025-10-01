26d0a2592e73d00a15c86bc1a04f338ac3ca151e
Khalid Morris <kha.morris@gmail.com>
2025-10-01 08:09:09 -0700

    Update Checkout test II

--- vite-milehighdna/src/pages/CheckoutInternational.jsx
+++ vite-milehighdna/src/pages/CheckoutInternational.jsx
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from "react";
-
-import React, { useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { useLocation } from "react-router-dom";
 
