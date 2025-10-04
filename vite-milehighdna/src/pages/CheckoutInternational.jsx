f7792a74268257a9fc07f355589ccd75b8e2cd81
Khalid Morris <kha.morris@gmail.com>
2025-10-04 05:24:36 -0600

    test

--- vite-milehighdna/src/pages/CheckoutInternational.jsx
+++ vite-milehighdna/src/pages/CheckoutInternational.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Helmet } from "react-helmet-async";
 import { useLocation } from "react-router-dom";
 import Select from "react-select";
-import * as countryRegionData from "country-region-data";
+import countryRegionData from "country-region-data";
 
 
 const CheckoutInternational = () => {
